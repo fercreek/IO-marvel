c98b6dfbfa39ad613edce16470700d738ce6d771
Fernando Contreras <fercreek@gmail.com>
2015-11-05 04:59:07 -0600

    Using Marvel API, get characters

--- www/js/app.js
+++ www/js/app.js
@@ -1,12 +1,48 @@
 (function(){
-  var app = angular.module('starter', ['ionic']);
+  var app = angular.module('starter', ['ionic', 'angular-md5']);
+
+  app.factory('MarvelService', MarvelService);
+  MarvelService.$inject = ['$http', '$q', 'md5'];
 
   app.controller('MarvelController', MarvelController);
+  MarvelController.$inject = ['MarvelService'];
 
-  MarvelController.$inject = [];
+  function MarvelController(MarvelService){
+    var vm = this;
+    vm.char = {};
 
-  function MarvelController(){
-    console.log("hellow");
+    MarvelService.getCharacters()
+      .then(function(data){
+      });
+  }
+
+  function MarvelService($http, $q, md5){
+    var publicKey = 'Your public key';
+    var privateKey = 'Your private Key';
+    var ts = Date.now();
+
+    var hash = md5.createHash(ts+privateKey+publicKey);
+    var baseUrl = 'http://gateway.marvel.com/v1/';
+
+    return {
+      getCharacters: getCharacters
+    };
+
+    function getCharacters() {
+      var def = $q.defer();
+
+      $http.get(baseUrl + 'public/characters', {
+          params: {
+            ts: ts,
+            apikey: publicKey,
+            hash: hash
+          }
+        })
+        .then(def.resolve)
+        .catch(def.reject);
+
+      return def.promise;
+    }
   }
 
   app.run(function($ionicPlatform) {
